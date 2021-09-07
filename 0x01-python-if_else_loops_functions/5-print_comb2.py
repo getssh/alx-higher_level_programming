550deb0b2c27b70f245ca5a06a84783a35d9995b
getssh <gtamrat33@gmail.com>
2021-09-08 01:18:03 +0300

    coma pep

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+
 for i in range(0, 100):
     if (i < 99):
         if (i < 10):
-            i = str(i);
-            i = i.zfill(2);
+            i = str(i)
+            i = i.zfill(2)
         print(i, end=", ")
-print("99");
-
+print(i)
