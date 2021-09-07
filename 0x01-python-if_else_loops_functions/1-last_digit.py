661500d87981e5365d7950b058f59b63e45b60a5
getssh <gtamrat33@gmail.com>
2021-09-08 00:49:13 +0300

    negatinve num handeld

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+x = number;
+if (number < 0):
+    number *= -1
 n = number
 l = n % 10
 
 if l > 5:
-    print("Last digit of {} is {} and is greater than 5".format(n, l))
+    print("Last digit of {} is {} and is greater than 5".format(x, l))
 elif l == 0:
-    print("Last digit of {} is {} and is 0".format(n, l))
+    print("Last digit of {} is {} and is 0".format(x, l))
 elif l < 6 and not(0):
-    print("Last digit of {} is {} and is less than 6 and not 0".format(n, l))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(x, l))
