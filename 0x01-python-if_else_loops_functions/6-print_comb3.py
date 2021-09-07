8c0a291b5ca32ac6101bd21807352e29a0cb5d69
getssh <gtamrat33@gmail.com>
2021-09-08 01:15:11 +0300

    pep

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 for num1 in range(0, 10):
-    num2 = num1 + 1;
+    num2 = num1 + 1
     for num3 in range(num2, 10):
         if (num1 == 8):
-            break;
+            break
         print("{}{}".format(num1, num3), end=", ")
 print("89")
