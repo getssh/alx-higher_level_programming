f6acd349286b6670844c7a150ea9fd37a35ef303
getssh <gtamrat33@gmail.com>
2021-09-13 23:11:20 +0300

    used reverse method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    i = -1
-    while (i >= -len(my_list)):
-        print("{:d}".format(my_list[i]))
-        i -= 1
+    my_list.reverse()
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
