577c8bcac9484c687699de11364a637e80b92a6f
getssh <gtamrat33@gmail.com>
2021-09-13 08:40:45 +0300

    pep8 fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,10 +3,6 @@
 
 def no_c(my_string):
     new_str = ""
-#    i = 0
-#while (i < len(my_string)):
-#       new_str.append(my_string[i])
-
     j = 0
     while (j < len(my_string)):
         if (my_string[j] != "c" and my_string[j] != "C"):
