629c6130226fbe63d6b9d17d635448278f2f3f0d
getssh <gtamrat33@gmail.com>
2021-09-13 21:33:59 +0300

    reverese

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,5 @@
 def print_reversed_list_integer(my_list=[]):
     i = -1
     while (i >= -len(my_list)):
-        print("{:d}".format(my_list[i]))
+        print("{}".format(my_list[i]))
         i -= 1
