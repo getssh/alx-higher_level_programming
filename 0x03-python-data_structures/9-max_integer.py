7ade55f9110bd168715d7740aa79b0374c29630b
getssh <gtamrat33@gmail.com>
2021-09-14 08:22:21 +0300

    max tring other metjid

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,9 @@ def max_integer(my_list=[]):
         return None
     _max = 0
     for i in range(len(my_list) - 1):
-        if (my_list[i] > my_list[i + 1]):
+        if my_list[i] > my_list[i + 1]:
             _max = my_list[i]
-            my_list[i] = my_list[i + 1]
-            my_list[i + 1] = _max
+            my_list[i + 1] = my_list[i]
+        else:
+            _max = my_list[i + 1]
     return _max
