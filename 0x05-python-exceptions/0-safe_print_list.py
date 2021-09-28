335c45153f39fd712c6c97be56b6d11eb87b9be6
root <root@DESKTOP-F2BLBCH.localdomain>
2021-09-28 05:13:59 +0300

    new line fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for y in range(x):
-            print(my_list[y], end="")
-        print()
-        return y + 1
-    except IndexError:
-        print()
-        return y
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print("")
+    return count
