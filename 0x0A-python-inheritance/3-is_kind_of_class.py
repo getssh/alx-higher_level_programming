ee101434265ad9e2e5295afd060407569ad61a50
root <root@DESKTOP-F2BLBCH.localdomain>
2021-10-21 10:45:28 +0300

    other method

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""class
+
+"""
 def is_kind_of_class(obj, a_class):
+    """fun to find out
+    bal hal
+    """
     if isinstance(obj, a_class):
         return True
     return False
