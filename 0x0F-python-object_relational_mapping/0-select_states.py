0ea2018293b50fe5f13a8f8b4616c5a7c097b5ae
root <root@DESKTOP-F2BLBCH.localdomain>
2021-12-20 00:42:56 +0300

    format corrected

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""script to print data from sql db
+"""
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     da = MySQLdb.connect("localhost", argv[1], argv[2], argv[3], port=3306)
     cur = da.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id")
     db_rows = cur.fetchall()
     for row in db_rows:
-        print("{}".format(row))
+        print(row)
     cur.close()
     da.close()
