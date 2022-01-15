84ee648881447b069640fd1d2832afd758dcbfe8
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-15 15:34:15 +0300

    post request with vars

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post with var
-curl -X POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
+curl -s -X POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
