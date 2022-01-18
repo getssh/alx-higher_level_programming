6ebf25a05fcf63b88cdc5a0cbb4925090a6ad235
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-18 11:23:43 +0300

    http error handle with urllib module

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''get request and handling http errors'''
+
+
+if __name__ == '__main__':
+    from urllib.request import urlopen
+    from urllib.error import URLError
+    from sys import argv
+
+    try:
+        with urlopen(argv[1]) as resp:
+            print(resp.read())
+    except URLError as e:
+        print(e.code)
