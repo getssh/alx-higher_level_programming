2d534f18a1d17c21a6d700bdd02647d816d2d6ea
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-18 10:38:16 +0300

    sending post req

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''sending a post request with email variable'''
+
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+    import urllib
+    from urllib.parse import urlencode
+    from sys import argv
+
+    email_value = {'email':argv[2]}
+    data = urlencode(email_value)
+    data = data.encode('utf-8')
+    req = Request(argv[1], data)
+    with urlopen(req) as resp:
+        print(resp.read())
