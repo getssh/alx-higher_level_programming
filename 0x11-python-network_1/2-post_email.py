b1025472128e690d9427e53e128425ed1a7875bc
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-18 10:46:16 +0300

    sending post req

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     email_value = {'email':argv[2]}
     data = urlencode(email_value)
-    data = data.encode('utf-8')
+    data = data.encode('UTF-8')
     req = Request(argv[1], data)
     with urlopen(req) as resp:
-        print(resp.read.decode())
+        print(resp.read.decode('UTF-8'))
