ba27a15354c365b54fd79bccc174f5df66688cc3
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-19 10:41:02 +0300

    post request to server

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''POST request using requests module'''
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    req = requests.post(argv[1], data={'email':argv[2]})
+    print(req)
