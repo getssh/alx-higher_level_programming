e875ecaa9441f2fdc2284e46210e1435bcf6ad3e
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-21 09:35:23 +0300

    arguments outof range got fiexed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,14 +6,16 @@ if __name__ == '__main__':
     import requests
     from sys import argv
 
-    req = requests.post('http://0.0.0.0:5000/search_user')
+    if len(argv) >= 2:
+        data = {'q': argv[1]}
+    else:
+        data = ""
+    req = requests.post('http://0.0.0.0:5000/search_user', data=data)
     print(req.json())
     try:
-        if len(argv) >= 2:
-            q = argv[1]
-            data = req.json()
-            print('[{}] {}'.format(q['id'], q['name']))
-        if len(argv) < 2 or req.json() == None:
+        j_data = req.json()
+        print('[{}] {}'.format(q['id'], q['name']))
+        j_data == None:
             print('No result')
     except Exception:
         print('Not a valid JSON')
