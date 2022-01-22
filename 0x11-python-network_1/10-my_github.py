f6c7fe8a90da6f2845742ed6efda6e396b9cbf63
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-22 09:26:47 +0300

    getting user id from githun

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''getting user info from github using requests module'''
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    token = argv[2]
+    user = argv[1]
+    query_url = f"https://api.github.com/users/{user}"
+    params = {
+        "state": "open",
+    }
+    headers = {'Authorization': f'token {token}'}
+    r = requests.get(query_url, headers=headers, params=params)
+    data =r.json()
+
+    print(data['id'])
